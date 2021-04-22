4521385888e164c6fee195c385a1d1364e2172ec
jungjaehi <damnyomoonwon1003@gmail.com>
2021-04-22 23:42:22 +0900

    ControlFlow

--- src/ControlFlow/If_else.kt
+++ src/ControlFlow/If_else.kt
@@ -1,2 +1,17 @@
 package ControlFlow
 
+import java.util.*
+
+fun main() {
+    var scanner : Scanner = Scanner(System.`in`)
+    var a : Int = scanner.nextInt()
+    var b : Int = scanner.nextInt()
+    var max : Int?
+    if (a < b)
+        max = b
+    else
+        max = a
+
+    println(max)
+
+}
